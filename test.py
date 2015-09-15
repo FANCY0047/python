35141e781c5b0b12411c3e6ef6e431a73462c6d2
= <=>
2015-09-15 21:25:21 +0800

    first test

---
+++ test.py
@@ -0,0 +1,13 @@
+#print all prime number between 2 and 100
+
+import math
+
+
+for num in range(2,100):
+    
+    for sub in range(2,int(math.ceil(math.sqrt(num)))):
+        if(num%sub == 0):
+            break
+    else:
+        print num,
+            
